4763bfd1a13fc6e561cb3f974e11fd3c97bc5ba2
SariaTita <Sariaabbas12345@gmail.com>
2023-01-03 22:00:13 +0000

    -

--- 0x01-python-if_else_loops_functions/list.h
+++ 0x01-python-if_else_loops_functions/list.h
@@ -1,4 +1,4 @@
-#ifndef LISTS_H
+B#ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +9,7 @@
 * @next: points to the next node
 *
 * Description: singly linked list node structure
-* for Holberton project
+* for MAIN project
 */
 typedef struct listint_s
 {
