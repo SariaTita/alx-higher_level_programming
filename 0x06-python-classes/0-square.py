bfd34ec2e96f39f6ae024b41ea9e628f0f172add
SariaTita <Sariaabbas12345@gmail.com>
2023-01-24 22:36:33 +0000

    -

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Square class """
+"""Defines a square """
+
+
 class Square:
-        """ empty class Square that defines a square """
-            pass
+    """Does nothing"""
+    pass
