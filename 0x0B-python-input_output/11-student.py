28d4544f4670545fcda179a69a28cc21dd237607
SariaTita <Sariaabbas12345@gmail.com>
2023-02-07 15:04:55 +0000

    -

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,7 +6,7 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student                           
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
